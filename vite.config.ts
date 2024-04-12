98c3cd11e13465cf548dc9b0463e54e5ba9f861d
Kagiso Moepya <kagisomoepya19@gmail.com>
2024-04-12 06:05:11 +0200

    :rocket: Predeploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ const root = resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Resume-Creator/',
   plugins: [react()],
   resolve: {
     alias: {
