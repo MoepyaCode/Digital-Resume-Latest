4f1fa8b859af98f6df39975f6b4cfe110122a124
Kagiso Moepya <kagisomoepya19@gmail.com>
2024-04-16 17:26:15 +0200

    :art: Navigation and Scrolling: Updates navigation box-shadow and scroll-padding-top based on screen width

--- src/templates/default/navbar/index.tsx
+++ src/templates/default/navbar/index.tsx
@@ -5,10 +5,50 @@ import { HomeData, Navigate } from "@templates/default/config";
 import { NavigateButton } from "../components";
 
 export function NavBar() {
+  const navRef = React.useRef<HTMLDivElement>(null);
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const [displayMenu, setDisplayBurgerMenu] = React.useState(false);
 
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
+
+    function handleNavBoxShadow() {
+      if (navRef.current) {
+        if (window.scrollY > 0) {
+          navRef.current.style.boxShadow = "0 4px 6px rgba(0, 0, 0, 0.1)";
+        } else {
+          navRef.current.style.boxShadow = "none";
+        }
+      }
+    }
+
+    window.addEventListener("scroll", handleNavBoxShadow);
+    handleNavBoxShadow();
+
+    return () => window.removeEventListener("scroll", handleNavBoxShadow);
+
+  }, [])
+
+  React.useLayoutEffect(() => {
+
+    function handleScrollPaddingTop() {
+      const html = document.querySelector("html") as HTMLElement;
+
+      if (window.innerWidth <= 700 && navRef.current) {
+        const navHeight = navRef.current.clientHeight;
+        html.style.scrollPaddingTop = `${navHeight}px`;
+      } else {
+        html.style.scrollPaddingTop = "0px";
+      }
+    }
+
+    window.addEventListener("resize", handleScrollPaddingTop);
+    handleScrollPaddingTop();
+
+    return () => window.removeEventListener("resize", handleScrollPaddingTop);
+
+  }, [])
+
+  React.useLayoutEffect(() => {
     function handleResize() {
       if (window.innerWidth <= 700) {
         setDisplayBurgerMenu(true);
@@ -24,7 +64,7 @@ export function NavBar() {
   }, []);
 
   return (
-    <nav className={Styles.NavBar}>
+    <nav ref={navRef} className={Styles.NavBar}>
       <NavigateButton
         className={Styles.NavBar_landingPageBtn}
 
